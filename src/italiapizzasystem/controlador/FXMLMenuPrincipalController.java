1bb5a35c0676322276cd2b7d5bd61d021c3a5470
Gera-BG <bgerardoabraham@gmail.com>
2026-05-11 19:10:32 -0600

    Se agrega los compoenentes visuales a la interfaz del menú principal

--- src/italiapizzasystem/controlador/FXMLMenuPrincipalController.java
+++ src/italiapizzasystem/controlador/FXMLMenuPrincipalController.java
@@ -3,7 +3,10 @@ package italiapizzasystem.controlador;
 import italiapizzasystem.persistencia.pojo.Empleado;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -16,6 +19,8 @@ public class FXMLMenuPrincipalController implements Initializable {
      * Initializes the controller class.
      */
     private Empleado empleado;
+    @FXML
+    private Label lbEmpleado;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
@@ -23,6 +28,27 @@ public class FXMLMenuPrincipalController implements Initializable {
     
     public void inicializarInformacion(Empleado empleado){
         this.empleado = empleado;
+        lbEmpleado.setText("Bienvenido(a) "+empleado.getNombre()+" "+empleado.getaPaterno()+" "+empleado.getaMaterno());
+    }
+
+    @FXML
+    private void btnClicCerrarSesion(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnClicAdministracion(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnClicAyuda(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnClicInventarios(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnClicPedidos(ActionEvent event) {
     }
     
 }
