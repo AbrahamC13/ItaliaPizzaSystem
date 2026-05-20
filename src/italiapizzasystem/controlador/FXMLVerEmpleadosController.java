5f93e3ca769b9090c61086758640cd9f4b25590c
Gera-BG <bgerardoabraham@gmail.com>
2026-05-20 17:46:57 -0600

    Se agrega funcionalidad al botón Regresar de la interfaz Ver empleados

--- src/italiapizzasystem/controlador/FXMLVerEmpleadosController.java
+++ src/italiapizzasystem/controlador/FXMLVerEmpleadosController.java
@@ -1,12 +1,22 @@
 
 package italiapizzasystem.controlador;
 
+import italiapizzasystem.ItaliaPizzaSystem;
+import italiapizzasystem.utilidad.Utilidad;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -17,7 +27,7 @@ public class FXMLVerEmpleadosController implements Initializable {
 
     @FXML
     private TextField tfNombre;
-
+    private static final Logger LOGGER = Logger.getLogger(FXMLVerEmpleadosController.class.getName());
     /**
      * Initializes the controller class.
      */
@@ -28,6 +38,24 @@ public class FXMLVerEmpleadosController implements Initializable {
 
     @FXML
     private void btnClicRegresar(ActionEvent event) {
+        try {
+            Stage escenarioBase = (Stage) tfNombre.getScene().getWindow();
+            FXMLLoader cargador = new FXMLLoader(ItaliaPizzaSystem.class.getResource("vista/FXMLMenuPrincipal.fxml"));
+            Parent vista = cargador.load();
+            FXMLMenuPrincipalController controlador = cargador.getController();
+            Scene escenaMenu = new Scene(vista);
+            escenarioBase.setScene(escenaMenu);
+            escenarioBase.setTitle("Menu Principal");
+            escenarioBase.show();
+        } catch (IOException ex) {
+            Utilidad.mostrarAlertaSimple(Alert.AlertType.ERROR, "Error al volver al menú principal.", ex.getMessage());
+            LOGGER.log(Level.SEVERE, "Error al cargar FXMLMenuPrincipal", ex);
+            ex.printStackTrace();
+        }catch(Exception ex){
+            Utilidad.mostrarAlertaSimple(Alert.AlertType.ERROR, "Error general", ex.getMessage());
+            LOGGER.log(Level.SEVERE, "Error general capturado en btnClicRegresar", ex);
+            ex.printStackTrace();
+        }
     }
 
     @FXML
