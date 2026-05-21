0547cc1914ba366ab5adcfce3025d1edd6ea73d6
Gera-BG <bgerardoabraham@gmail.com>
2026-05-20 18:15:11 -0600

    Se añade funcionalidad al botón Cancelar de la interfaz AgregarEmpleado

--- src/italiapizzasystem/controlador/FXMLAgregarEmpleadoController.java
+++ src/italiapizzasystem/controlador/FXMLAgregarEmpleadoController.java
@@ -1,12 +1,22 @@
 
 package italiapizzasystem.controlador;
 
+import italiapizzasystem.ItaliaPizzaSystem;
+import italiapizzasystem.utilidad.Utilidad;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -37,7 +47,7 @@ public class FXMLAgregarEmpleadoController implements Initializable {
     private TextField tfUsuario;
     @FXML
     private TextField tfContrasenia;
-
+     private static final Logger LOGGER = Logger.getLogger(FXMLAgregarEmpleadoController.class.getName());
     /**
      * Initializes the controller class.
      */
@@ -48,6 +58,24 @@ public class FXMLAgregarEmpleadoController implements Initializable {
 
     @FXML
     private void btnClicCancelar(ActionEvent event) {
+         try {
+            Stage escenarioBase = (Stage) tfNombre.getScene().getWindow();
+            FXMLLoader cargador = new FXMLLoader(ItaliaPizzaSystem.class.getResource("vista/FXMLVerEmpleados.fxml"));
+            Parent vista = cargador.load();
+            FXMLVerEmpleadosController controlador = cargador.getController();
+            Scene escenaEmpleados = new Scene(vista);
+            escenarioBase.setScene(escenaEmpleados);
+            escenarioBase.setTitle("Ver empleados");
+            escenarioBase.show();
+        } catch (IOException ex) {
+            Utilidad.mostrarAlertaSimple(Alert.AlertType.ERROR, "Error al volver a ver a los empleados.", ex.getMessage());
+            LOGGER.log(Level.SEVERE, "Error al cargar FXMLVerEmpleados", ex);
+            ex.printStackTrace();
+        }catch(Exception ex){
+            Utilidad.mostrarAlertaSimple(Alert.AlertType.ERROR, "Error general", ex.getMessage());
+            LOGGER.log(Level.SEVERE, "Error general capturado en btnClicCancelar", ex);
+            ex.printStackTrace();
+        }
     }
 
     @FXML
