536f33e8c95e4210afd52fc10bbfce993ded0b45
Gera-BG <bgerardoabraham@gmail.com>
2026-05-20 18:49:59 -0600

    Se añade la funcionalidad para registrar al empleado y el método en DAO para realizarlo

--- src/italiapizzasystem/persistencia/dao/EmpleadoDAO.java
+++ src/italiapizzasystem/persistencia/dao/EmpleadoDAO.java
@@ -120,4 +120,72 @@ public class EmpleadoDAO {
         return contraseniaEncontrada;
     
     }
+    
+    public static boolean registrarEmpleado(Empleado empleado) throws SQLException{
+        boolean resultado = false;
+        String insert = "INSERT INTO empleados (nombre, apellido_paterno, apellido_materno, ciudad, codigo_postal, "
+                   + "direccion, email, telefono, rol, usuario, contrasenia, status) "
+                   + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        Connection conexionBD = ConexionBD.abrirConexion();
+        if(conexionBD != null){
+            PreparedStatement pstmt = conexionBD.prepareStatement(insert); 
+            pstmt.setString(1, empleado.getNombre());
+            pstmt.setString(2, empleado.getaPaterno());
+            pstmt.setString(3, empleado.getaMaterno());
+            pstmt.setString(4, empleado.getCiudad());
+            pstmt.setString(5, empleado.getCodigoPostal());
+            pstmt.setString(6, empleado.getDireccion());
+            pstmt.setString(7, empleado.getEmail());
+            pstmt.setString(8, empleado.getTelefono());
+            pstmt.setString(9, empleado.getRol());
+            pstmt.setString(10, empleado.getUsuario());
+            pstmt.setString(11, empleado.getContrasenia());
+            pstmt.setBoolean(12, empleado.isStatus());
+            int filasAfectadas = pstmt.executeUpdate();
+            resultado = filasAfectadas > 0;
+        }else{
+            throw new SQLException("Error al conectar a la BD");
+        }
+            
+       return resultado;
+    }
+    
+    //Restricción: No puede haber dos empleados con el mismo usuario
+     public static boolean validarEmpleadoExistente(String usuario) throws SQLException {
+         boolean resultado = false;
+        String consulta = "SELECT COUNT(*) FROM empleados WHERE usuario = ?";
+        Connection conexionBD = ConexionBD.abrirConexion();
+        if(conexionBD != null){
+            PreparedStatement pstmt = conexionBD.prepareStatement(consulta);
+            pstmt.setString(1, usuario);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                resultado = count > 0;
+            }
+        }else{
+            throw new SQLException("Error al conectar a la BD");
+        }
+       
+        return resultado;
+    }
+    //Restricción: No puede haber dos empleados con el mismo correo electrónico 
+    public static boolean validarEmpleadoExistentePorEmail(String email) throws SQLException {
+        boolean resultado = false;      
+        String consulta = "SELECT COUNT(*) FROM empleados WHERE email = ?";
+        Connection conexionBD = ConexionBD.abrirConexion();
+        if(conexionBD != null){
+            PreparedStatement pstmt = conexionBD.prepareStatement(consulta);
+            pstmt.setString(1, email);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                resultado = rs.getInt(1) > 0;
+            }
+        }else{
+            throw new SQLException("Error al conectar a la BD.");
+        }
+       
+        return  resultado;
+    }
 }
